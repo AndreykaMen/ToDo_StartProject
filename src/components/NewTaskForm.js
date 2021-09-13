5959b8bea5e3f653661c1c1b2d231fbe83d5209b
AndreykaMen <aodinmg@gmail.com>
2021-09-14 00:30:16 +0600

    Изменил обьект state. Изменил удаление тасков с помощью filter(). Добавил функцию добавления тасков.createTodoItem()

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,6 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 
-const NewTaskForm = () => {
-  return <input className="new-todo" placeholder="What needs to be done?" autoFocus/>;
+export default class NewTaskForm extends Component {
+  state = {
+    description: ''
+  };
+
+  onLabelChange = ( event ) => {
+    this.setState( {
+      description: event.target.value
+    } );
+  };
+
+  onSubmit = ( event ) => {
+    event.preventDefault();
+    this.props.onItemAdded( this.state.description );
+    this.setState( {
+      description: ''
+    } );
+  };
+
+  render() {
+    return (
+        <form className="new-todo-form" onSubmit={ this.onSubmit }>
+          <input className="new-todo" placeholder="What needs to be done?"
+                 autoFocus
+                 onChange={ this.onLabelChange }
+                 value={ this.state.description }
+          />
+        </form>
+    );
+  }
 };
-export default NewTaskForm;
--- src/components/app.js
+++ src/components/app.js
@@ -4,39 +4,62 @@ import NewTaskForm from "./NewTaskForm";
 import Footer from "./Footer";
 
 export default class App extends Component {
+  maxId = 0;
   state = {
     taskData: [
-      {
-        id: 0,
-        done: false,
-        classItem: null,
-        description: "Completed task",
-        createdItem: "created 17 seconds ago",
-        edit: false
-      },
-      {
-        id: 1,
-        done: false,
-        classItem: null/*"editing"*/,
-        description: "Editing task",
-        createdItem: "created 5 minutes ago",
-        edit: true
-      },
-      {
-        id: 2,
-        done: false,
-        classItem: null,
-        description: "Active task",
-        createdItem: "created 5 minutes ago",
-        edit: false
-      }
+      // {
+      //   id: 0,
+      //   done: false,
+      //   classItem: null,
+      //   description: "Completed task",
+      //   createdItem: "created 17 seconds ago",
+      //   edit: false
+      // },
+      // {
+      //   id: 1,
+      //   done: false,
+      //   classItem: null/*"editing"*/,
+      //   description: "Editing task",
+      //   createdItem: "created 5 minutes ago",
+      //   edit: true
+      // },
+      // {
+      //   id: 2,
+      //   done: false,
+      //   classItem: null,
+      //   description: "Active task",
+      //   createdItem: "created 5 minutes ago",
+      //   edit: false
+      // }
     ]
   };
-  filterData = [
+  filterButtonData = [
     { id: 'b0', classSelect: "selected", filterName: 'All' },
     { id: 'b1', classSelect: null, filterName: 'Active' },
     { id: 'b2', classSelect: null, filterName: 'Completed' }
   ];
+
+  createTodoItem( description ) {
+    return {
+      description,
+      important: false,
+      classItem: null,
+      createdItem: "created 5 minutes ago",
+      done: false,
+      edit: false,
+      id: this.maxId++
+    };
+  };
+
+  addItem = ( text ) => {
+    const newItem = this.createTodoItem( text );
+    this.setState( ( { taskData } ) => {
+      const newArr = [...taskData, newItem];
+      return {
+        taskData: newArr
+      };
+    } );
+  };
 
   onToggleCompleted = ( id, classItem ) => {
     const newStateTask = this.state.taskData.map( ( elem ) => {
@@ -55,10 +78,13 @@ export default class App extends Component {
   onDeleteItem = ( id ) => {
     this.setState( ( { taskData } ) => {
       const idx = taskData.findIndex( ( el ) => el.id === id );
-      const [...copyArray] = taskData;
-      copyArray.splice( idx, 1 );
+      // const [...copyArray] = taskData;
+      // copyArray.splice( idx, 1 );
+      taskData = taskData.filter( ( item, itemIdx ) => {
+        return itemIdx !== idx;
+      } );
       return {
-        taskData: copyArray
+        taskData: taskData
       };
     } );
   };
@@ -68,14 +94,14 @@ export default class App extends Component {
         <section className="todoapp">
           <header className="header">
             <h1>todos</h1>
-            <NewTaskForm/>
+            <NewTaskForm onItemAdded={ this.addItem }/>
           </header>
           <section className="main">
             <TaskList task={ this.state.taskData }
                       onToggleCompleted={ this.onToggleCompleted }
                       onDeleted={ this.onDeleteItem }
             />
-            <Footer filter={ this.filterData }/>
+            <Footer filter={ this.filterButtonData }/>
           </section>
         </section>
     );
